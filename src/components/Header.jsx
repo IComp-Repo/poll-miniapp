2354edbc896679f3623be3dbfdd5671fbc87c830
Samuel Monteiro <gomessamuel.14@gmail.com>
2025-06-05 19:34:50 -0400

    bugfix: remove i18n

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,8 @@
 import Image from 'next/image';
-import { useTranslation } from 'react-i18next';
 import logo from '../assets/logo.png'; // Ajuste o caminho se necessário
 import styles from "../styles/useGlobal.module.css"; // Ajuste o caminho se necessário
 
 export default function Header({title}) {
-  const { t } = useTranslation();
 
   return (
     <header className={styles.waveHeader}>
