ade581243a6ad6a414486f35078c9a01711b7988
Pratamartin <martinhoprata95@gmail.com>
2025-09-04 15:09:38 -0400

    feat: implement dashboard data fetching and activity handling

--- src/components/UltimasAtividades.jsx
+++ src/components/UltimasAtividades.jsx
@@ -40,9 +40,7 @@ const columns = [
     },
     {
         name: 'Respostas',
-        selector: row => row.answers
-            ? row.answers
-            : 'Nenhuma resposta',
+        selector: row => row.answers ? row.answers : 'Nenhuma resposta',
     },
     {
         name: 'Ações',
@@ -52,7 +50,7 @@ const columns = [
                 onClick={() => alert(`Ação para ${row.title}`)}
             >
                 Detalhes
-                <ChartBarIcon width={20} height={20} style={{marginLeft: '6px'}} />
+                <ChartBarIcon width={20} height={20} style={{ marginLeft: '6px' }} />
             </button>
         ),
         ignoreRowClick: true,
@@ -61,34 +59,32 @@ const columns = [
     },
 ];
 
-
-export default function UltimasAtividades({data}) {
+export default function UltimasAtividades({ data }) {
     return (
-        <>
-            <div
-                className={`${styles.cardHover} shadow rounded`}
-                style={{
-                    backgroundColor: '#fff',
-                    width: '100%',
-                    maxWidth: '1300px',
-                    padding: '16px',
-                    borderRadius: '8px',
-                    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-                    margin: '16px auto',
-                    fontSize: '14px',
-                    color: '#333',
-                    whiteSpace: 'normal',
-                }}
-            >
-                <DataTable
-                    customStyles={customStyles}
-                    columns={columns}
-                    data={data}
-                    highlightOnHover
-                    striped
-                />
-
-            </div>
-        </>
+        <div
+            className={`${styles.cardHover} shadow rounded`}
+            style={{
+                backgroundColor: '#fff',
+                width: '100%',
+                maxWidth: '1300px',
+                padding: '16px',
+                borderRadius: '8px',
+                boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+                margin: '16px auto',
+                fontSize: '14px',
+                color: '#333',
+                whiteSpace: 'normal',
+            }}
+        >
+            <DataTable
+                keyField="id"
+                customStyles={customStyles}
+                columns={columns}
+                data={data ?? []}
+                highlightOnHover
+                striped
+                noDataComponent="Sem atividades recentes"
+            />
+        </div>
     );
 }
